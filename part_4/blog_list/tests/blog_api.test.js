2e4b46166374c6711985b1672420668bf3ec511b
RobinQuanNguyen <nguyenbaoquan2004@gmail.com>
2024-01-05 13:47:35 +0200

    update for part 4

--- part_4/blog_list/tests/blog_api.test.js
+++ part_4/blog_list/tests/blog_api.test.js
@@ -295,7 +295,7 @@ describe('when there is initially one user in db', () => {
         expect(usersAtEnd).toEqual(usersAtStart)
     })
   })
- 
+
 afterAll(async () => {
     await mongoose.connection.close()
 })
